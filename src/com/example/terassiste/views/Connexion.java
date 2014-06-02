e5de4726493cfd23fd46b538e0f310ee21b68235
Shinthujan SIVAKUMAR <shinthujan.sivakumar@gmail.com>
2014-06-02 23:38:33 +0200

    Updating connexion

--- src/com/example/terassiste/views/Connexion.java
+++ src/com/example/terassiste/views/Connexion.java
@@ -10,13 +10,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.terassiste.R;
-import com.example.terassiste.R.id;
-import com.example.terassiste.R.layout;
 import com.example.terassiste.http.HttpClient;
 
 public class Connexion extends Activity {
 	private static final String TAG = "Connexion";
-	private static final String URL = "http://www.yourdomain.com:80";
+	private static final String URL = "http://terassiste.alwaysdata.net/connect";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,10 +32,6 @@ public class Connexion extends Activity {
 			jsonObject.put("id", id.getText());
             jsonObject.put("pass", pass.getText());
 
-			JSONObject header = new JSONObject();
-			header.put("deviceType","Android"); // Device type
-			header.put("deviceVersion","4.4"); // Device OS version
-			jsonObject.put("header", header);
 
 			Log.i(TAG, jsonObject.toString(2));
 
@@ -46,7 +40,9 @@ public class Connexion extends Activity {
 		}
 
 		JSONObject jsonRec = HttpClient.SendHttpPost(URL, jsonObject);
-
+		Log.i("TAG", jsonRec.toString());
+		//pass.setText(jsonRec.toString());
+		
     
     }
     
